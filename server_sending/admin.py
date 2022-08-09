dc3b8a38dcd04161e50d61dfbb42e7cff5879ef7
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-08-09 14:40:33 +0300

    finish models

--- server_sending/admin.py
+++ server_sending/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from server_sending.models import Sending, Client, Massage
+
+
+class SendingAdmin(admin.ModelAdmin):
+    list_display = ('start', 'stop',
+                    'massage', 'tag', 'code',)
+    search_fields = ('id',)
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('tel_number', 'mobile_code', 'tag',)
+
+
+class MassageAdmin(admin.ModelAdmin):
+    list_display = ('pub_date', 'sending', 'client', 'status',)
+
+
+admin.site.register(Sending, SendingAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Massage, MassageAdmin)
--- server_sending/models.py
+++ server_sending/models.py
@@ -1,6 +1,6 @@
 from django.core.validators import RegexValidator
 from django.db import models
-
+import pytz
 
 class StatusChoices(models.TextChoices):
     SEND = 'S', 'Send'
@@ -8,18 +8,24 @@ class StatusChoices(models.TextChoices):
 
 
 class Sending(models.Model):
-    star = models.DateTimeField()
+    start = models.DateTimeField()
     stop = models.DateTimeField()
     massage = models.TextField()
-    tag = models.ForeignKey('Client', on_delete=models.CASCADE, related_name='tags')
-    status = models.ForeignKey('Massage', on_delete=models.CASCADE, related_name='statuses')
+    tag = models.ForeignKey('Client', on_delete=models.CASCADE, related_name='tags', blank=True,
+                            null=True)
+    code = models.ForeignKey('Client', on_delete=models.CASCADE, related_name='codes',
+                             blank=True, null=True)
 
 
 class Client(models.Model):
     regex = RegexValidator(regex=r'^7\d{10}$')
-    tel_number = models.IntegerField(validators=[regex])
-    mobile_code = models.IntegerField()
+    tel_number = models.CharField(validators=[regex], unique=True, max_length=11, default='7')
+    mobile_code = models.CharField(max_length=4)
     tag = models.CharField(max_length=20)
+    time_zone = models.CharField(max_length=50, choices=[(timezone, timezone) for timezone in pytz.common_timezones], default='UTC')
+
+    def __str__(self):
+        return f'{self.tag}, {self.mobile_code}'
 
 
 class Massage(models.Model):
